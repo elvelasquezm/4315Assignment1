731a9346debc5ff8ce65707bade468d0a3657ef9
elvelasquezm <42789035+elvelasquezm@users.noreply.github.com>
2018-09-06 14:57:44 -0500

    Update Assign1.cpp

--- Assign1.cpp
+++ Assign1.cpp
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -8,64 +9,114 @@
 
 using namespace std;
 
-map<char, int> GenerateMap(map<char, int> newMap){
+/**
+ * Gets the name of the file sent through the command line
+ *
+ * @param commandLineInput  holds the command line arguments
+ * @return fileName         string that holds the name of the file
+ * @return exit(0)          exits program if no file was sent
+ */
+string GetFileName(char** commandLineInput){
+    string fileName;
 
-}
-
-void interpretLine(string line, int& letterCount, int& nameCount){
-
-}
-
-int main(int argc, char** argv){
-    string fileName, buffer;
-    map<char, int> startsWith;
-    vector<vector<char>> names(1);
-    int j = 0;
-    double avg = 0, letterCount = 0, nameCount = 1;
-    char letter = 'A';
-    for(int i = 0; i < 26; i++){
-        startsWith[letter] = 0;
-        letter++;
+    //returns commandline argument as string if it exists
+    //else exit program
+    if(commandLineInput[1] != NULL) {
+        fileName = commandLineInput[1];
+        return fileName;
+    } else {
+        cout<<"No input file\n";
+        exit(0);
     }
+}
 
-    if(argv[1] != NULL)
-        fileName = argv[1];
-    else{
-        cout<<"no input file\n";
+/**
+ * Generates a map with alpha keys
+ *
+ * @return newAlphaMap  the generated map
+ */
+map<char, int> GenerateAlphaMap(){
+    //map we are about to manipulate
+    map<char, int> newAlphaMap;
+
+    //iterates through alphabet and adds each letter as key for our map
+    for(char c = 'A'; c <= 'Z'; c++)
+        newAlphaMap[c] = 0;
+
+    return newAlphaMap;
+}
+
+/**
+ * Gets the number of names, and how many begin with each alpha letter,
+ * as well as the total number of alpha letters
+ *
+ * @param inputFile     the file that we are reading from
+ * @param namesCount    the number of names in the file
+ * @param letterCount   the number of alpha letters in the file
+ * @param startsWith    a map whose values represent the number of names
+ *                      that begin with a specific alpha letter
+ *                      (represented by the keys)
+ */
+void GetFileInfo(ifstream& inputFile, int& namesCount, int& letterCount, map<char, int>& startsWith){
+    //buffer for holding each line in file
+    string buffer;
+
+    //iterates through each lines in file
+    while(getline(inputFile, buffer))
+        //iterates through each character in line
+        for(int i =0; i < buffer.size(); i++){
+            //if character is uppercase then new name has started
+            //else name has more letters
+            if(isupper(buffer[i])){
+                namesCount++;
+                startsWith[buffer[i]]++;
+                letterCount++;
+            } else if(islower(buffer[i]))
+                letterCount++;
+        }
+}
+
+/**
+ * Prints the info obtained from the {@code GetInfo} fucntion
+ * The parameters carry over the same meaning as in the {@code GetInfo} info
+ *
+ * @param letterCount
+ * @param namesCount
+ * @param startsWith
+ */
+void PrintInfo(int letterCount, int namesCount, map<char, int> startsWith){
+    //print average number of letters per name
+    cout<<"Average number of letters per name: "<<(double)(letterCount/namesCount)<<endl;
+
+    //print number of names that begin wit each letter of the alphabet
+    cout<<"Names starting with ___\n";
+    for (char c = 'A'; c <= 'Z' ; c++)
+        if(startsWith[c] != 0)
+            cout<<c<<": "<<startsWith[c]<<endl;
+}
+
+/**
+ * Main function
+ */
+int main(int argc, char** argv){
+
+    //opens the input file to read
+    ifstream inputFile;
+    inputFile.open(GetFileName(argv));
+    if(!inputFile){
+        cout<<"File not found\n";
         return 0;
     }
 
-    try{
-        ifstream inputFile;
-        inputFile.open(fileName);
-        while(getline(inputFile, buffer)){
-            for(int i = 0; i<buffer.size(); i++){
-                if(buffer[i] == ',') {
-                    i++;
-                    j++;
-                    names.resize(++nameCount);
-                } else {
-                    names[j].push_back(buffer[i]);
+    //instantiates variables for getting info from file
+    int namesCount = 0, letterCount =0;
+    map<char, int> startsWith = GenerateAlphaMap();
 
-                }
-            }
-        }
-        --nameCount;
-        for(int i = 0; i < names.size(); i++){
-            letterCount += names[i].size();
-            startsWith[names[i][0]]++;
-        }
-        cout<<(letterCount / nameCount)<<endl;
-        letter = 'A';
-        for(int i = 0; i < 26; i++){
-            if(letter]!=0){
-                cout<<letter<<": "<<startsWith[letter]<<endl;
-            }
-            letter++;
-        }
-        inputFile.close();
-    }
-    catch(exception& e){
-        cout<<"File not found\n";
-    }
+    //gets info from file and displays it
+    GetFileInfo(inputFile, namesCount, letterCount, startsWith);
+    PrintInfo(letterCount, namesCount, startsWith);
+
+    //ends program
+    inputFile.close();
+    return 0;
 }
